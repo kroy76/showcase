c253884f45dc51c2ef77745b612151133b053c0f
Kaushik <kausik.roychowdhury@gmail.com>
2024-08-06 05:44:29 +0000

    remove java template from repo

--- backstage/catalog/templates/apps/javarest-k8s/content/.devcontainer/post-create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-echo "post-create create" >> ~/status
- 
-# This runs each time the container creates.
- 
-echo "post-create complete" >> ~/status
--- backstage/catalog/templates/apps/javarest-k8s/content/.devcontainer/post-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-echo "post-start start" >> ~/status
- 
-# This runs each time the container starts.
- 
-echo "post-start complete" >> ~/status
